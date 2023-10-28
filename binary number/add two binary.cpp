2f89a5997a0d940f6e43668b08f26c088e1f5676
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2023-10-28 17:08:28 +0530

    Create add two binary.cpp

---
+++ binary number/add two binary.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+int reverse(int n)
+{
+    int ans = 0;
+    while(n>0)
+    {
+        int lastdigit = n%10;
+        ans = ans*10 + lastdigit;
+        n/= 10;
+    }
+    return ans;
+}
+int addbinary(int a, int b)
+{
+    int ans =0;
+    int carry = 0;
+    while(a>0 && b>0){
+        if(a%2 ==0 && b%2 == 0){
+            ans = ans*10+ carry;
+            carry = 0;
+        }
+        else if((a%2==0 && b%2==1)|| (a%2==1 && b%2==0))
+        {
+            if(carry ==1)
+            {
+                ans = ans*10 + 0;
+                carry = 1;
+            }
+            else
+            {
+                ans = ans *10 + 1;
+                carry = 0;
+            }
+        }
+        else
+        {
+            ans = ans*10 + carry;
+            carry = 1;
+        }
+        a/= 10;
+        b/= 10;
+    }
+    while(a>0)
+    {
+        if(carry == 1)
+        {
+            if(a%2 == 1)
+            {
+                ans = ans*10 + 0;
+                carry = 1;
+            }
+            else
+            {
+                ans = ans*10 + 1;
+                carry = 0;
+            }
+        }
+        else
+        {
+            ans = ans*10 + (a%2);
+        }
+        a/= 10;
+    }
+    while(b>0)
+    {
+        if(carry == 1)
+        {
+            if(b%2 == 1)
+            {
+                ans = ans*10 + 0;
+                carry = 1;
+            }
+            else
+            {
+                ans = ans*10 + 1;
+                carry = 0;
+            }
+        }
+        else
+        {
+            ans = ans*10 + (b%2);
+        }
+        b/= 10;
+    }
+    if(carry == 1)
+    {
+        ans = ans*10 + 1;
+    }
+    ans = reverse(ans);
+    return ans;
+}
+int main() 
+{
+    int x, y;
+    cout<<"enter two binary number";
+    cin>>x>>y;
+    cout<<addbinary(x,y);
+    return 0;
+}
