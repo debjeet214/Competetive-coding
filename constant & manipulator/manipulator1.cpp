9660bf19ad97205455a1980da769c0951714c9d1
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-08 14:38:05 +0530

    Create manipulator1.cpp

---
+++ constant & manipulator/manipulator1.cpp
@@ -0,0 +1,14 @@
+// use of manipulator in c++
+#include <iostream> 
+
+using namespace std;  
+int main( ) {  
+   int a = 50;
+   int c, b;
+   cout << "value of b is  " << endl; 
+   cout<<"\n";
+   cout<<"\n";
+   cin>>b;
+   c= a+b;
+   cout << "value of c is " << c<< endl;  
+}  
