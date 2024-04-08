173a91a57e70f64737a0a1d46b3f968e39e7ebf9
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-08 14:36:50 +0530

    Create constant variable.cpp

---
+++ constant & manipulator/constant variable.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std; 
+const int global_const = 100; 
+void printGlobalConstant(){
+    cout<<"The global constant is: "<<global_const<<endl;
+    } 
+    int main() { 
+        printGlobalConstant();
+        return 0; 
+    } 
