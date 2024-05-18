547e2b2023748aa855c8bf9da66d67d7a21e0eb7
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-05-18 10:37:06 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,3 +1,8 @@
+// Breadth First Search (BFS) is a graph traversal algorithm that explores all the vertices in a graph at the current depth before moving on to the vertices at the next depth level. It starts at a specified vertex and visits all its neighbors before moving on to the next level of neighbors. BFS is commonly used in algorithms for pathfinding, connected components, and shortest path problems in graphs.
+
+//  
+
+
 #include <iostream>
 #include <queue>
 #include <vector>
