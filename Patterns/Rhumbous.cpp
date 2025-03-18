60d8c03413581ea045bb8f92785c89c3d96cde33
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2025-03-18 13:11:59 +0530

    Create Rhumbous.cpp

---
+++ Patterns/Rhumbous.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"enter the number of rows you want : ";
+   cin>>n;
+   for(int i = 1; i<=n ; i++){
+       for(int j = 1; j<=n-i; j++){
+          cout<<" "; 
+       }
+       for(int j = 1; j<=n; j++){
+          cout<<"*"; 
+       }
+       cout<<endl;
+   }
+    return 0;
+}
