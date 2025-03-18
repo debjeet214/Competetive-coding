cb12d117318c57f84ca39e8e38c84aa3887c341a
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2025-03-18 13:13:19 +0530

    Create Right Sided triangle.cpp

---
+++ Patterns/Right Sided triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"enter the number of rows you want"<<endl;
+   cin>>n;
+   for(int i = 1; i<=n ; i++){
+       for(int j = 1; j<= n; j++){
+           if(j<= n-i){
+           cout<<" ";
+           }
+           else 
+           cout<<"*";
+       }
+       cout<<endl;
+   }
+    return 0;
+}
